e81a8be9bb24e603b5162d7ed9f5df7edf213813
Azim0nt <kktotov457@gmail.com>
2024-01-15 17:20:48 +0500

    Savign result

--- furniture/src/components/Home/index.jsx
+++ furniture/src/components/Home/index.jsx
@@ -1,14 +1,32 @@
 import './style.scss'
 import CarouselComponent from '../common/CarouselComponent/CarouselComponent.jsx'
-import SofaImg_1 from '../../assets/images/sofa1.jpg'
-import SofaImg_2 from '../../assets/images/sofa2.jpg'
-import SofaImg_3 from '../../assets/images/sofa3.jpg'
+import SofaImg_1 from '../../assets/images/11.jpg'
+import SofaImg_2 from '../../assets/images/22.jpg'
+import SofaImg_3 from '../../assets/images/33.jpg'
 function Home() {
     const images = [SofaImg_1, SofaImg_2, SofaImg_3]
     return (
         <div className="home-wrapper">
             <CarouselComponent images={images} blurred>
-                
+            <div className="text-wrapper">
+                    <p id="title">
+                        Exclusive Deals Of <br />
+                        Furniture <span>Collection</span>
+                    </p>
+                    <p id="desc">
+                        Exprore different catalog for our showroom and online service <br />
+                        client happyness for our furniture this 
+                    </p>
+
+                    <div className="buttons">
+                        <button className="red-btn">
+                            Buy Now 
+                        </button>
+                        <button className="white-btn">
+                            Explore 
+                        </button>
+                    </div>
+                </div>
                 </CarouselComponent>
         </div>
     );
--- furniture/src/components/common/CarouselComponent/CarouselComponent.jsx
+++ furniture/src/components/common/CarouselComponent/CarouselComponent.jsx
@@ -6,16 +6,14 @@ import { Carousel } from 'react-responsive-carousel';
 
 function CarouselComponent(props) {
 
-    const imgStyle = {
-        filter: props.blurred ? " brightness(0.5) " : "none"
-    }
 
     return (
         <div className="carousel-c-wrapper">
             <div className="carousel-c-children">
                 {props.children}
             </div>
-            <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
+            <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false} showArrows={false}>
+                
                 {
                     props.images && props.images.map((img, index) => {
                         return (            
@@ -23,9 +21,9 @@ function CarouselComponent(props) {
                                 <img 
                                     src={img} 
                                     alt={"Carousel-img-" + index} 
-                                    width={"100%"}
-                                    height={600}
-                                    style={imgStyle}
+                                    width={'100%'}
+                                    height={700}
+                                   
                                 />
                             </div>
                         )
